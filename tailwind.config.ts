73fa38f3549c14b934442853ec43221f5d67985e
zer0 <maelcolome.pro@icloud.com>
2024-10-14 21:07:53 +0200

    ✅  F01: APP > Page.tsx > Verification of component functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -6,6 +7,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
