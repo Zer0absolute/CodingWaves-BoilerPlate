2e4aeb486ec755108e96280537b72d6da3af326a
zer0 <maelcolome.pro@icloud.com>
2024-10-25 03:31:39 +0200

    ✨ F08: app > dashboard > create layout for dashboard and create DashboardPage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,14 @@
+"use client"
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 export default function DashboardPage() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <div>Dashboard</div>
   )
