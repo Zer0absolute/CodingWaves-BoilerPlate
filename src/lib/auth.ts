ad1be856540b99d22c29e90c3bd51f47bbb5702a
zer0 <maelcolome.pro@icloud.com>
2024-10-25 03:24:34 +0200

    ♻️ F08: src > lib > Add session callback to enrich session.user with user.id and user.image"

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,17 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import prisma from "./prisma";
 
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
   theme: {
     logo: "/images/logo.svg",
   },
   adapter: PrismaAdapter(prisma),
-  providers: [Google]
+  providers: [Google],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      session.user.image = user.image;
+      return session;
+    },
+  },
 });
